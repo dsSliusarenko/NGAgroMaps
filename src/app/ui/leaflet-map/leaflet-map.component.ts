4c27cfa58a033f782eca808f7b98d709475e8686
Dmytro Sliusarenko <dmytro.sliusarenko@holdmycode.com>
2023-07-03 11:54:43 -0600

    added files to leaflet mapr component

---
+++ src/app/ui/leaflet-map/leaflet-map.component.ts
@@ -0,0 +1,39 @@
+import {AfterViewInit, Component} from '@angular/core';
+import * as L from "leaflet";
+
+@Component({
+  selector: 'nga-leaflet-map',
+  templateUrl: './leaflet-map.component.html',
+  styleUrls: ['./leaflet-map.component.scss']
+})
+export class LeafletMapComponent implements AfterViewInit {
+  private map!: L.Map | L.LayerGroup<any>;
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 51.049999, -114.066666 ],
+      zoom: 14
+    });
+
+    const tiles = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    });
+
+    const objectsNames = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-labels/{z}/{x}/{y}{r}.{ext}', {
+      attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      minZoom: 0,
+      maxZoom: 20,
+      //@ts-ignore
+      ext: 'png'
+    });
+
+    tiles.addTo(this.map);
+    objectsNames.addTo(this.map);
+  }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+}
