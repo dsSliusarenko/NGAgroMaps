594961e734aeecf7d1d05c66f7977f574efb9132
Dmytro Sliusarenko <dmytro.sliusarenko@holdmycode.com>
2023-07-03 16:07:31 -0600

    added Output with lastCoords in leaflet-map component

--- src/app/overview/map/map.component.ts
+++ src/app/overview/map/map.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {MapConfig} from "../../ui/leaflet-map/map";
+import * as L from "leaflet";
 
 @Component({
   selector: 'nga-map',
@@ -9,7 +10,11 @@ import {MapConfig} from "../../ui/leaflet-map/map";
 export class MapComponent {
 
   mapConfig: MapConfig = {
-    center: [51.049999, -114.066666],
+    center: [ 51.191408092498484, -114.09355093741783],
     zoom: 14
+  }
+
+  printLastCoords (lastCoords: L.LatLng): void {
+    console.log(lastCoords)
   }
 }
--- src/app/ui/leaflet-map/leaflet-map.component.ts
+++ src/app/ui/leaflet-map/leaflet-map.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, Input} from '@angular/core';
+import {AfterViewInit, Component, EventEmitter, Input, Output} from '@angular/core';
 import * as L from "leaflet";
 import {MapConfig} from "./map";
 
@@ -10,27 +10,33 @@ import {MapConfig} from "./map";
 export class LeafletMapComponent implements AfterViewInit {
   @Input() mapConfig!: MapConfig;
 
-  private map!: L.Map | L.LayerGroup<any>;
+  @Output() lastCoords: EventEmitter<L.LatLng> = new EventEmitter();
+
+  private map!: L.Map;
 
   private initMap(): void {
     this.map = L.map('map', this.mapConfig);
 
-    const tiles = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    const tiles: L.TileLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
       maxZoom: 18,
       minZoom: 3,
       attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
     });
 
-    const objectsNames = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-labels/{z}/{x}/{y}{r}.{ext}', {
-      attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      minZoom: 0,
-      maxZoom: 20,
-      //@ts-ignore
-      ext: 'png'
-    });
+    // const objectsNames: L.TileLayer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-labels/{z}/{x}/{y}{r}.{ext}', {
+    //   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    //   minZoom: 0,
+    //   maxZoom: 20,
+    //   //@ts-ignore
+    //   ext: 'png'
+    // });
 
     tiles.addTo(this.map);
-    objectsNames.addTo(this.map);
+    // objectsNames.addTo(this.map);
+    
+    this.map.on('dragend', () => {
+      this.lastCoords.emit(this.map.getCenter());
+    });
   }
 
   ngAfterViewInit(): void {
