91ecc2165bb9bd2a499270f463e6374e59a568d4
Dmytro Sliusarenko <dmytro.sliusarenko@holdmycode.com>
2023-07-03 21:30:45 -0600

    created profile section component

---
+++ src/app/ui/profile-section/profile-section.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProfileSectionComponent } from './profile-section.component';
+
+@NgModule({
+  declarations: [
+    ProfileSectionComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ProfileSectionComponent
+  ]
+})
+export class ProfileSectionModule { }
